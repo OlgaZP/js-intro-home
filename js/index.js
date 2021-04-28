a69347b931a389307c0f5fe58dba74e3ef70d70b
OlgaK <oskozlova@gmail.com>
2021-04-28 22:47:10 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+console.group("Variable tasks");  
+const a = 5;
+const b = 7.7;
+console.log('a*b :>> ', a*b);
+
+const c = 25;
+const d = 4.5;
+console.log('c/d :>> ', c/d);
+
+const e = 10;
+const f = 100;
+console.log('num e+f :>> ', e+f);
+
+const e = '10';
+const f = '100';
+console.log('string e+f :>> ', e+f);
+
+const num = 11;
+const bool = true;
+const str = "java script";
+const str100 = "100";
+console.log('number 11 :>> ', num);
+console.log('boolean :>> ', bool);
+console.log('string :>> ', str);
+console.log('string 100 :>> ', str100);
+
+
+console.groupEnd; 
