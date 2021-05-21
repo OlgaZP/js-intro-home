3bd8671cc4602064dc6ad2447560432be5d9f4e0
OlgaK <oskozlova@gmail.com>
2021-05-21 19:37:18 +0300

    add user tasks without getFullName in users-home.js

---
+++ js/users-home.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//initialaize 100 users-array with random data
+function User (name, surname, age, isMale, email, isSubscribe) {
+    this.firstName = name;
+    this.lastName = surname;
+    this.age = age;
+    this.isMale = isMale;
+    this.email = email;
+    this.isSubscribe = isSubscribe;
+}
+
+// 1.1 Для примера с последней части занятия прописать метод getFullName() (возвращает строку с полным именем) для юзера. Общую логику (т.е. указанный метод) вынести в прототип. 
+const protoUser = new User();
+
+protoUser.getFullName = function () {
+    return this.firstName+' '+this.lastName; 
+}
+
+User.prototype = protoUser;
+
+const users = [];
+
+for (let i = 0; i < 100; i++) {
+    const user = new User(
+        `Username${i}`,
+        `Usersurname${i}`,
+        Math.trunc(Math.random() * 99 + 1),
+        Math.random() > 0.5,
+        `usermail${i}@gmail.com`,
+        Math.random() > 0.5,
+    );
+    users.push(user);    
+}
+//console.table(users);
+console.log('users[5].getFullName():>> ', users[5].getFullName());
+
+//выводим полные имена пользователей
+console.log('Test getFullName :>> ');
+//console.table(users.map(user.getFullName));
+
+// 1.2 Получить массив полных имен лиц женского пола школьного возраста (6 - 18 лет).
+function isSchoolgirl (user) {
+    return !user.isMale && user.age>= 6 && user.age <= 18;
+}
+console.log('Show only schoolgirl :>> ');
+const userSchoolgirl = users.filter(isSchoolgirl);
+console.table(userSchoolgirl);
+
+// 1.3 Проверить, есть ли среди пользователей пользователь с email`ом usermail99@gmail.com
+console.log('Is register email usermail99@gmail.com:>> ');
+const findEmail = users.find(item => item.email === 'usermail99@gmail.com');
+console.table(findEmail);
+
+// 1.4 Проверить, все ли пользователи подписаны (subscribed).
+console.log('Is register email usermail99@gmail.com:>> ');
+const isAllSubscribe = users.every(item => item.isSubscribe === true);
+console.table(isAllSubscribe);
